eb763062cd2b5881166bb72f24eddd3606e68ec3
Markéta <meggy.calabkova@gmail.com>
2023-08-30 13:22:59 +0200

    fix tests with Sphinx 7.2

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ import os
 import tempfile
 
 import pytest
-from sphinx.testing.path import path
+from pathlib import Path
 
 pytest_plugins = 'sphinx.testing.fixtures'
 
@@ -19,11 +19,10 @@ collect_ignore = ['roots']
 
 @pytest.fixture(scope='session')
 def sphinx_test_tempdir():
-    return path(
+    return Path(
         os.environ.get('SPHINX_TEST_TEMPDIR',
-                       tempfile.mkdtemp(prefix='apidoc-'))).abspath()
-
+                       tempfile.mkdtemp(prefix='apidoc-'))).resolve()
 
 @pytest.fixture(scope='session')
 def rootdir():
-    return path(os.path.dirname(__file__) or '.').abspath() / 'roots'
+    return Path(os.path.dirname(__file__) or '.').resolve() / 'roots'
